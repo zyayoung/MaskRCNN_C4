cb09f0ddc07ca9368fb30a7d5cdeaf8662951147
Zhang Yuang <1328410180@qq.com>
2019-06-25 12:14:23 +0800

    fix

--- test.py
+++ test.py
@@ -105,7 +105,7 @@ def parse_args():
     parser.add_argument('--img-pixel-means', type=str, default='(0.0, 0.0, 0.0)')
     parser.add_argument('--img-pixel-stds', type=str, default='(1.0, 1.0, 1.0)')
     parser.add_argument('--rpn-feat-stride', type=int, default=16)
-    parser.add_argument('--rpn-anchor-scales', type=str, default='(8, 16, 32, 64, 128)')
+    parser.add_argument('--rpn-anchor-scales', type=str, default='(2, 4, 8, 16, 32)')
     parser.add_argument('--rpn-anchor-ratios', type=str, default='(0.5, 1, 2)')
     parser.add_argument('--rpn-pre-nms-topk', type=int, default=6000)
     parser.add_argument('--rpn-post-nms-topk', type=int, default=300)
